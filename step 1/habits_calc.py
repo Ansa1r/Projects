bb5297ebcbc5556fb25f6f10a4d1cf862e93410f
Ansa1r <maksim140606@gmail.com>
2026-05-20 14:58:42 +0300

    Habits calc 2
    Изменения:
    - Убраны лишние строки, например импорты
    - Добавлено главное меню
    - Доработка выбора формата времени
    - Расширенная функция для работы с часовым форматом

--- step 1/habits_calc.py
+++ step 1/habits_calc.py
@@ -1,10 +1,15 @@
-from sys import flags
-
-from unicodedata import name
-
-
 def hello(): #Приветствие
     print("Приветствую в калькуляторе привычек.")
+
+def choose():
+    voit = int(input("Выберите что вы хотите: \n"
+                                "1. Рассчёт времени привычки \n"
+                                "2. Выйти \n"))
+    if voit == 1:
+        habits()
+    elif voit == 2:
+        print("Завершение программы")
+        exit()
 
 def habits():
     name = input("Введите имя привычки:")
@@ -15,9 +20,9 @@ def habits():
                                 "2. Минуты \n"
                                 "3. Секунды \n"))
         if habits_time == 1:
-            habits_calc_hour()
+            habits_calc_hour(name)
             flag = True
-            return name, habits_time, habits_calc_hour()
+            return name, habits_time
 
         elif habits_time == 2:
             habits_calc_min()
@@ -33,16 +38,28 @@ def habits():
             print("Ошибка, введите цифру от 1 до 3.")
 
 
-def habits_calc_hour(name): # основная функция
+def habits_calc_hour(name):
     t = int(input("Введите время: "))
     month_hour = t * 30
     year_hour = month_hour * 12
     if t > 8:
-        print("Как ты собрался тратить", t, "часов в день на" f'{name}' "?")
+        print("Как ты собрался тратить", t, "часов в день на " f'{name}' "?")
+        return habits()
+    elif 8 >= t >= 5:
+        print(f'{name}' " каждый день по", t, "часов довольно серьёзно. "
+            "Это", month_hour, "часов в месяц и", year_hour, "часов в год. "
+              "Уверен, что справишься?")
+        return habits()
+    elif 5 > t >= 2:
+        print(f'{name}' " каждый день по", t, "часа в день звучит как хорошая цель. "
+            "Это", month_hour, "часов в месяц и", year_hour, "часов в год. ")
+        return habits()
+    elif t == 1:
+        print(f'{name}' " каждый день по", t, "часу это прекрасное начало. "
+            "Это", month_hour, "часа в месяц и", year_hour, "часов в год. ")
         return habits()
 
-
-def habits_calc_min(): # основная функция
+def habits_calc_min():
     t = int(input("Введите время: "))
     month_hour = (t * 30)/60
     year_hour = (month_hour * 12)
@@ -53,7 +70,7 @@ def habits_calc_min(): # основная функция
 
 
 
-def habits_calc_sec(): # основная функция
+def habits_calc_sec():
     t = int(input("Введите время: "))
     month_hour = (t * 30)/60
     year_hour = (month_hour * 12)
@@ -64,4 +81,4 @@ def habits_calc_sec(): # основная функция
 
 if __name__ == "__main__":
     hello()
-    habits()
+    choose()
